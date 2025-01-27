8c57ed84fb41a68c10d2ca2a62866550e2528f99
NorahMT <nouraaltwijriii@gmail.com>
2025-01-27 17:26:59 +0300

    app.js file

--- app.js
+++ app.js
@@ -3,7 +3,23 @@ const express = require('express');
 const expressLayouts = require('express-ejs-layouts');
 
 const app = express();
-const port = 5000 || process.env.PORT;
+const port = 3000 || process.env.PORT;
 
 app.use(express.urlencoded({extended: true}));
 app.use(express.json());
+
+//static folder
+app.use(express.static('public'));
+
+//Templating Engine
+app.use(expressLayouts);
+app.set('layouyt', './layouts/main');
+app.set('view engine', 'ejs');
+
+app.get('/' , function(req, res){
+    res.render('index', { title: "Home Page" });
+})
+
+app.listen(port, ()=>{
+    console.log(`App listening on port ${port}`);
+})

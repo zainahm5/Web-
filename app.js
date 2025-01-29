33b9d71a8bd1d98cf740412464f9c09d0c0016de
falmofeez <444006548@sm.imamu.edu.sa>
2025-01-29 19:37:40 +0300

    database connected

--- app.js
+++ app.js
@@ -3,13 +3,16 @@ require('dotenv').config();
 const express = require('express');
 const expressLayouts = require('express-ejs-layouts');
 
+const connectDB = require('./server/config/db');
+
 const app = express();
 const port = 3000 || process.env.PORT;
 
+// Connect to the database
+connectDB();
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-
 
 // Static Files
 app.use(express.static('public'));

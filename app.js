bc21e42f0527e95c2a0fcd10310f88a68d06460b
NouraAbuthnain <Noura7abuthnainl@gmail.com>
2025-01-29 13:23:59 +0300

    Server configuration file

---
+++ app.js
@@ -0,0 +1,27 @@
+require('dotenv').config();
+
+const express = require('express');
+const expressLayouts = require('express-ejs-layouts');
+
+const app = express();
+const port = 5000 || process.env.PORT;
+
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+
+// Static Files
+app.use(express.static('public'));
+
+// Templating Engine
+app.use(expressLayouts);
+app.set('layout', './layouts/main');
+app.set('view engine', 'ejs');
+
+// Routes
+app.use('/', require('./server/routes/index'));
+
+app.listen(port, () => {
+    console.log(`App listening on port ${port}`);
+});

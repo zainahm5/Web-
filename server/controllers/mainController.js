4113183d60a1afdb2e8cf2c4609d3c1e891ed45e
NorahMT <nouraaltwijriii@gmail.com>
2025-01-29 12:52:35 +0300

    restart

--- server/controllers/mainController.js
+++
@@ -1,7 +0,0 @@
-exports.getHomePage = async (req, res) => {
-   const locals = {
-        title: 'EventServe',
-        description: 'Volunteering hub for seasonal events'
-    }
-    res.render('index', locals);
-}
--- server/routes/index.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const homeController = require('../controllers/mainController');
-
-router.get('/', mainController.getHomePage);
-
-module.exports = router;

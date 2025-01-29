099f0f150b0937277f76eecac3c648e7d458e12e
NouraAbuthnain <Noura7abuthnainl@gmail.com>
2025-01-29 13:30:04 +0300

    The server folder

---
+++ server/controllers/mainController.js
@@ -0,0 +1,85 @@
+// GET Homepage
+exports.homepage = async (req, res) => {
+  const locals = {
+    title: 'SeasonServe',
+    description: 'Volunteering Opportunities Website'
+  };
+  // Check if the user is logged in
+  if (req.isAuthenticated && req.isAuthenticated()) {
+    // User is logged in, render with main layout
+    res.render('index', {
+      locals,
+      layout: '../views/layouts/main'
+    });
+  } else {
+    // User is not logged in, render with front-page layout
+    res.render('index', {
+      locals,
+      layout: '../views/layouts/front-page'
+    });
+  }
+};
+
+
+// GET About
+exports.about = async (req, res) => {
+  const locals = {
+    title: 'About - SeasonServe',
+    description: 'Volunteering Opportunities Website'
+  }
+  res.render('about', locals);
+}
+
+// GET Events
+exports.events = async (req, res) => {
+  const locals = {
+    title: 'Events - SeasonServe',
+    description: 'Volunteering Opportunities Website'
+  }
+  res.render('events', locals);
+}
+
+// GET Details
+exports.details = async (req, res) => {
+  const locals = {
+    title: 'Details - SeasonServe',
+    description: 'Volunteering Opportunities Website'
+  }
+  res.render('details', locals);
+}
+
+// GET Profile
+exports.profile = async (req, res) => {
+  const locals = {
+    title: 'Profile - SeasonServe',
+    description: 'Volunteering Opportunities Website'
+  }
+  res.render('profile', locals);
+}
+
+// GET Sign In
+exports.signin = async (req, res) => {
+  const locals = {
+    title: 'Sign In - SeasonServe',
+    description: 'Volunteering Opportunities Website'
+  }
+  res.render('signin', locals);
+}
+
+// GET Sign Up
+exports.signup = async (req, res) => {
+  const locals = {
+    title: 'Sign Up - SeasonServe',
+    description: 'Volunteering Opportunities Website'
+  }
+  res.render('signup', locals);
+}
+
+// GET Logout
+exports.logout = async (req, res) => {
+  const locals = {
+    title: 'Logout - SeasonServe',
+    description: 'Volunteering Opportunities Website'
+  }
+  res.render('logout', locals);
+}
---
+++ server/routes/index.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const router = express.Router();
+const mainController = require('../controllers/mainController');
+
+// App Routes
+router.get('/', mainController.homepage);
+router.get('/about', mainController.about);
+router.get('/events', mainController.events);
+router.get('/details', mainController.details);
+router.get('/profile', mainController.profile);
+router.get('/signin', mainController.signin);
+router.get('/signup', mainController.signup);
+router.get('/logout', mainController.logout);
+
+module.exports = router;

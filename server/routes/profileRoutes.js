e061d234067c322885944659b2de3f953eac3d7c
zainahm5 <148555756+zainahm5@users.noreply.github.com>
2025-01-30 20:52:27 +0300

    Create profileRoutes.js

---
+++ server/routes/profileRoutes.js
@@ -0,0 +1,12 @@
+// routes/profileRoutes.js
+const express = require('express');
+const router = express.Router();
+const profileController = require('../controllers/profileController');
+
+// GET request for the profile page
+router.get('/profile', profileController.getProfile);
+
+// POST request to update the profile
+router.post('/profile', profileController.updateProfile);
+
+module.exports = router;
